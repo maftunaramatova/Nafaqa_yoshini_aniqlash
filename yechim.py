9b80799281b463e3c60bcfb634da2196845c12e2
ramatovamaftuna <djumaniyazovumarjon@gmail.com>
2026-01-28 17:50:03 +0500

    Update yechim.py
    yangi fayl kiritildi

--- yechim.py
+++ yechim.py
@@ -1,122 +1,81 @@
 ##1-Masala:
-# class CartItem:
-#     def __init__(self, name, price, quantity, discount):
-#         # Mahsulot nomi
-#         self.name = name
-#         # Mahsulot narxi
-#         self.price = price
-#         # Mahsulot miqdori
-#         self.quantity = quantity
-#         # Chegirma foizi
-#         self.discount = discount
+# class Transport:
+#     def __init__(self,model:str,yil:int):
+#         self.model = model
+#         self.yil = yil
 #
-#     def total_cost(self):
-#         # Chegirma qo'llangan umumiy narxni hisoblash
-#         return self.price * self.quantity * (1 - self.discount / 100)
+#     def malumot(self)-> str:
+#         return f"Model: {self.model} Yil: {self.yil}"
 #
+# class Avtomobil(Transport):
+#     def __init__(self, model: str , yil:int, yonilgi_turi: str):
+#         super().__init__(model,yil)
+#         self.yonilgi_turi = yonilgi_turi
 #
-# # Oziq-ovqat mahsulotlari uchun sinf
-# class FoodItem(CartItem):
-#     def total_cost(self):
-#         # Asosiy sinfdagi hisoblash metodidan foydalanish
-#         cost = super().total_cost()
-#         # Formatlangan natijani qaytarish
-#         return f"Oziq-ovqat mahsuloti: {self.name} -> {cost:.2f}$"
+#     def malumot(self):
+#         baza = super().malumot()
+#         return f"{baza} , Yonilg'i:{self.yonilgi_turi}"
 #
+# class Avtobus(Transport):
+#     def __init__(self, model: str , yil:int,orinlar_soni: str ):
+#         super().__init__(model,yil)
+#         self.orinlar_soni = orinlar_soni
+#     def malumot(self):
+#         baza = super().malumot()
+#         return f"{baza} , O'rindiqlar soni:{self.orinlar_soni}"
 #
-# # Oziq-ovqat bo'lmagan mahsulotlar uchun sinf
-# class NonFoodItem(CartItem):
-#     def total_cost(self):
-#         # Asosiy sinfdagi hisoblash metodidan foydalanish
-#         cost = super().total_cost()
-#         # Formatlangan natijani qaytarish
-#         return f"Oziq-ovqat mahsuloti: {self.name} -> {cost:.2f}$"
+# a = Avtomobil("Cobalt",2022,"benzin")
+# print(a.malumot())
 #
-#
-# # Savatchadagi mahsulotlar ro'yxati
-# items = [
-#     FoodItem("Olma", 1.5, 3, 10),
-#     FoodItem("Non", 2.0, 2, 5)
-# ]
-#
-# # Barcha mahsulotlar bo'yicha umumiy summani hisoblash
-# total_sum = 0
-#
-# # Har bir mahsulotning narxini chiqarish
-# for item in items:
-#     print(item.total_cost())
-#     total_sum += item.price * item.quantity * (1 - item.discount / 100)
-#
-# # Umumiy savatcha narxini chiqarish
-# print("Umumiy summa:", round(total_sum, 2), "$")
-#
+# b = Avtomobil("Isuzu",2018,"40")
+# print(b.malumot())
 #
 
-##2-Masala:
-# class Vehicle:
-#     def __init__(self, name, distance, fuel_efficiency, fuel_price):
-#         self.name = name
-#         self.distance = distance
-#         self.fuel_efficiency = fuel_efficiency
-#         self.fuel_price = fuel_price
+##2-masala:
+# class Kitob:
+#     def __init__(self, nom,muallif,yil):
+#         self.nom = nom
+#         self.muallif = muallif
+#         self.yil = yil
 #
-#     def trip_cost(self):
-#         return self.distance * self.fuel_efficiency * self.fuel_price
+#     def taqdimot(self):
+#         return f"Kitob nomi:{self.nom},Kitob muallifi:{self.muallif},Yili: {self.yil}"
 #
+# class ElektronKitob(Kitob):
+#     def __init__(self, nom,muallif,yil,fayl_hajmi_mb):
+#         super().__init__(nom,muallif,yil)
+#         self.fayl_hajmi_mb = fayl_hajmi_mb
 #
-# class Car(Vehicle):
-#     def show_cost(self):
-#         cost = self.trip_cost()
-#         return f"Transport nomi: {self.name} -> {cost:.2f}"
+#     def taqdimot(self):
+#         baza = super().taqdimot()
+#         return f"{baza}, [Elektron :{self.fayl_hajmi_mb}]"
 #
+# class AudioKitob(Kitob):
+#     def __init__(self, nom,muallif,yil,davomiylik_soat):
+#         super().__init__(nom, muallif, yil)
+#         self.davomiylik_soat = davomiylik_soat
 #
-# class Motorcycle(Vehicle):
-#     def show_cost(self):
-#         cost = self.trip_cost()
-#         return f"Transport nomi: {self.name} -> {cost:.2f}"
+#     def taqdimot(self):
+#         baza = super().taqdimot()
+#         return f"{baza}, [Audio :{self.davomiylik_soat} soat]"
 #
+# e = ElektronKitob("Python asoslari", "Ali", 2023, 5)
+# print(e.taqdimot())
 #
-# items = [
-#     Car("Avto", 130, 5, 8),
-#     Motorcycle("Motosikl", 78, 7, 9)
-# ]
-#
-# total_cost = 0
-# for item in items:
-#     print(item.show_cost())
-#     total_cost += item.trip_cost()
-#
-# print("Umumiy xarajat:", round(total_cost, 2))
+# a = AudioKitob("O'tkan kunlar", "Abdulla Qodiriy", 2020, 12)
+# print(a.taqdimot())
 
 ##3-Masala:
-# Hayvonlar = [{"It", 500}, {"Mushuk", 200}]
-# class Pet:
-#     def __init__(self,name, daily_food):
-#         self.name = name
-#         self.daily_food = daily_food
+# class Xodim:
+#     def __init__(self, ism,asosiy_maosh):
+#         self.ism = ism
+#         self.asosiy_maosh = asosiy_maosh
 #
+#     def oylik(self):
+#         return self.asosiy_maosh
 #
-#     def fedding_plan(self):
-#         return self.daily_food
-#
-#
-# class Dog(Pet):
-#     def fedding_plan(self):
-#         amount = super().fedding_plan()
-#         return f"It nomi;{self.name},kunlik ovqat miqdori;{amount} gramm"
-#
-#
-# class Cat(Pet):
-#     def fedding_plan(self):
-#         amount = super().fedding_plan()
-#         return f"Mushuk nomi;{self.name},kunlik ovqat miqdori;{amount} gramm"
-#
-# if __name__ == '__main__':
-#     dog = Dog("Olapar", 500)
-#     cat = Cat("Mosh",200)
-#
-#     print(dog.fedding_plan())
-#     print(cat.fedding_plan())
+#     def malumot(self):
+
 
 
 
